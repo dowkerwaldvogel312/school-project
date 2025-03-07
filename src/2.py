28663c20f94796819c72aa2a1a95db086aebf18f
dowkerwaldvogel312 <dowkerwaldvogel312@gmail.com>
2025-03-07 19:37:35 +0000

    Add 2.py

---
+++ src/2.py
@@ -0,0 +1,10 @@
+import random
+
+def get_random_string(length):
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    result = ''
+    for i in range(length):
+        result += random.choice(letters)
+    return result
+
+get_random_string(10)
