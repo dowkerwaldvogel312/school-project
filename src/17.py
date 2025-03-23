84f151dfb0fcb70c9830f84713b4aa0e8d815cc5
dowkerwaldvogel312 <dowkerwaldvogel312@gmail.com>
2025-03-23 22:52:41 +0000

    Add 17.py

---
+++ src/17.py
@@ -0,0 +1,12 @@
+def print_message():
+    """
+    This function prints a message.
+    
+    Example usage:
+    >>> print_message()
+    Hello World!
+    """
+    print("Hello World!")
+
+if __name__ == "__main__":
+    print_message()  # You can call the function if you want to
